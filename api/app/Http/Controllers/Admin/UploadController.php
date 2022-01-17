84e697576f2a9fa781bb91439b482c0a01a2c2cf
hunter <1039526501@qq.com>
2022-01-17 15:43:52 +0800

    添加上传文件try

--- api/app/Http/Controllers/Admin/UploadController.php
+++ api/app/Http/Controllers/Admin/UploadController.php
@@ -7,23 +7,28 @@ use App\Http\Controllers\Apis;
 
 class UploadController extends Apis{
     public function index(){
-        if (request()->hasFile('file')&&request()->file('file')->isValid()) {
-            $file=request()->file('file');
+        try{
+            if (request()->hasFile('file')&&request()->file('file')->isValid()) {
+                $file=request()->file('file');
 
-            $allowed_extensions = ["xlsx"];
+                $allowed_extensions = ["xlsx"];
 
-            if (!in_array($file->getClientOriginalExtension(), $allowed_extensions)) {
-                throw new \Exception('请上传xlsx文件');
+                if (!in_array($file->getClientOriginalExtension(), $allowed_extensions)) {
+                    throw new \Exception('请上传xlsx文件');
+                } else {
+                    $destinationPath = 'storage/uploads/'; //public 文件夹下面建 storage/uploads 文件夹
+                    $extension = $file->getClientOriginalExtension();
+                    $fileName=md5(time().rand(1, 1000)).'.'.$extension;
+                    $file->move($destinationPath, $fileName);
+                    $filePath = $destinationPath.$fileName;
+                    return $this->response(['url'=>$filePath]);
+                }
             } else {
-                $destinationPath = 'storage/uploads/'; //public 文件夹下面建 storage/uploads 文件夹
-                $extension = $file->getClientOriginalExtension();
-                $fileName=md5(time().rand(1, 1000)).'.'.$extension;
-                $file->move($destinationPath, $fileName);
-                $filePath = $destinationPath.$fileName;
-                return $this->response(['url'=>$filePath]);
+                return '';
             }
-        } else {
-            return '';
+        }catch(\Exception $e){
+            return $e->getMessage();
         }
+        
     }
 }
