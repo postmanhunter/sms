ac6b32de183689a5a0a04ac63f5f69c2778ca848
hunter <1039526501@qq.com>
2022-02-15 15:51:10 +0800

    d

--- admin/src/utils/request.js
+++ admin/src/utils/request.js
@@ -53,24 +53,23 @@ service.interceptors.response.use(
     //   })
 
     //   // 50008: Illegal token; 50012: Other clients logged in; 50014: Token expired;
-    //   if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
-    //     // to re-login
-    //     MessageBox.confirm('You have been logged out, you can cancel to stay on this page, or log in again', 'Confirm logout', {
-    //       confirmButtonText: 'Re-Login',
-    //       cancelButtonText: 'Cancel',
-    //       type: 'warning'
-    //     }).then(() => {
-    //       store.dispatch('user/resetToken').then(() => {
-    //         location.reload()
-    //       })
-    //     })
-    //   }
-    //   return Promise.reject(new Error(res.message || 'Error'))
-    // } else {
-    //   return res
-    // }
-    return res;
-    },
+      if (res.code === 999999) {
+        // to re-login
+        MessageBox.confirm('You have been logged out, you can cancel to stay on this page, or log in again', 'Confirm logout', {
+          confirmButtonText: 'Re-Login',
+          cancelButtonText: 'Cancel',
+          type: 'warning'
+        }).then(() => {
+          store.dispatch('user/resetToken').then(() => {
+            location.reload()
+          })
+        })
+      } else {
+        return res;
+      }
+       
+    // return res;
+    // },
   error => {
     console.log('err' + error) // for debug
     Message({
@@ -80,6 +79,7 @@ service.interceptors.response.use(
     })
     return Promise.reject(error)
   }
+}
 )
 
 export default service
